201907c7003d21047a06be7a41f3de69f65d60b9
NAVENKUMAR D <naven.duraisamy5859@gmail.com>
2022-09-21 16:14:08 +0530

    added about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigateComponent } from './navigate/navigate.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigateComponent
+    NavigateComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      
+      {path:"about", component:AboutComponent},
+      {path:"**", redirectTo:"/about", pathMatch:"full"}
     ])
   ],
   providers: [],
